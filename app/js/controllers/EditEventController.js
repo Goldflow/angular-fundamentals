8ac324891836e087a2afed3be47b6e89b3d41f16
kdehbi <karim.dehbi@gmail.com>
2016-04-19 18:14:15 +0200

    created saving resource functionality, reformatted some code in EventData

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,16 +1,23 @@
 'use strict';
 
 eventsApp.controller('EditEventController',
-    function EditEventController($scope) {
+    function EditEventController($scope, eventData) {
+
+        $scope.event = {};
         //parameters:
         //1st parameter is the eventData filled in by user from the form
         //2nd parameter is whether or not the data is valid
         $scope.saveEvent = function(event, newEventForm) {
             if(newEventForm.$valid)
             {
-                window.alert('event ' + event.name + ' saved!');
+                //window.alert('event ' + event.name + ' saved!');
+                eventData.save(event).
+                    $promise
+                    .then(function(response) {console.log('success', response)})
+                    .catch(function(response) {console.log('failure',response)}
+                );
             }
-        }
+        };
 
         $scope.cancelEdit = function() {
             window.location = "/EventDetails.html"
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,27 +1,22 @@
 /**
  * Created by kdehbi on 15/04/2016.
  */
-//what parameters you pass in to the factory method
-//1) the name of the service (here 'eventData')
-//2) a function that returns the object that will become that service
 
 //here we inject resource
 eventsApp.factory('eventData', function ($resource) {
+    var resource = $resource('/data/event/:id', {id: '@id'});
+
     return {
         getEvent: function () {
 
-            //to make $resource work, we need to register the module with the app (in app.js here)
-            //based on restful architecture
-            //so assumes your service is using restbased architecture
-            return $resource('/data/event/:id', //this is the format of the (rest)url
-                {id:'@id'})//second parameter of the resource,
-                // is an object that specifies default values that are used in that route
-                // so > id in url will be replaced by id in object being saved/retrieved
-                .get({id:1}); //so we call get, we pass object to say we want object with id 1
+            return resource.get({id: 1}); //so we call get, we pass object to say we want object with id 1
+        },
+        save: function (event) {
+            event.id = 999;
+            return resource.save(event);
         }
-      }
+    }
 });
-
 
 
 /**
